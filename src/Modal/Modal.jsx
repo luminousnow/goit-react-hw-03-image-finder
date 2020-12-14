0f027c8c9d4540b9c700550b55d681787d3cb21d
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2020-12-14 22:49:26 +0200

    додає окремий root на модалку, додає закриття по ЕСК і Бекдропу

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
 import s from './Modal.module.css';
 
+const modalRoot = document.querySelector('#modal-root');
+
 export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.hendleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.hendleKeyDown);
+  }
+
+  hendleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  hendleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+
   render() {
-    return (
-      <div className={s.overlay}>
+    return createPortal(
+      <div className={s.overlay} onClick={this.hendleBackdropClick}>
         <div className={s.modal}>
           <img src="" alt="" />
         </div>
-      </div>
+      </div>,
+      modalRoot,
     );
   }
 }
