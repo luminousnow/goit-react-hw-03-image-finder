e94f9a212e83c3ce5a507a0d03ceece0f590685e
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2020-12-16 15:55:01 +0200

    прибирає перенесену розмітку

--- src/Components/ImageGallery/ImageGallery.jsx
+++ src/Components/ImageGallery/ImageGallery.jsx
@@ -4,46 +4,16 @@ import Loader from 'react-loader-spinner';
 import Button from '../Button/Button';
 
 export class ImageGallery extends Component {
-  state = {
-    // KEY: '19092084-bc4ccb70eacd908f2d855c18b',
-    // URL: 'https://pixabay.com/api/',
-    // imgCollection: null,
-    // pageNum: 1,
-    // perPage: 12,
-    showLoader: false,
-  };
-
-  // componentDidMount() {}
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   const prevImgName = prevProps.imgName;
-  //   const newImgName = this.props.imgName;
-  //   const { KEY, URL, perPage } = this.state;
-  //   let { pageNum } = this.state;
-
-  //   if (prevImgName !== newImgName) {
-  //     pageNum = 1;
-
-  //     this.setState({ showLoader: true });
-  //     fetch(
-  //       `${URL}?q=${newImgName}&page=${pageNum}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`,
-  //     )
-  //       .then(res => res.json())
-  //       .then(imgCollection =>
-  //         this.setState({ imgCollection: imgCollection.hits }),
-  //       )
-  //       .finally(() => this.setState({ showLoader: false }));
-  //   }
-  // }
+  state = {};
 
   render() {
-    console.log(this.state.imgCollection);
     const newImgName = this.props.imgName;
-    const { imgCollection, showLoader } = this.state;
+    const imgGallery = this.props.imgGallery;
+    const showLoader = this.props.showLoader;
 
     return (
       <>
-        {imgCollection && (
+        {imgGallery && (
           <div>
             <h1>{`Результати по: ${newImgName}`}</h1>
             <ul className={s.imageGallery}></ul>;
