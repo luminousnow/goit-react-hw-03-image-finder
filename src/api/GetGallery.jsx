1561d77d0079a2eb8ab4d26ee1ef6ce408e9b282
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2020-12-16 14:17:42 +0200

    переносить Fetch з ImageGallery

---
+++ src/api/GetGallery.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export class GetGallery extends Component {
+  state = {
+    KEY: '19092084-bc4ccb70eacd908f2d855c18b',
+    URL: 'https://pixabay.com/api/',
+    pageNum: 1,
+    perPage: 12,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevImgName = prevProps.imgName;
+    const newImgName = this.props.imgName;
+    const { KEY, URL, perPage } = this.state;
+    let { pageNum } = this.state;
+
+    if (prevImgName !== newImgName) {
+      pageNum = 1;
+      fetch(
+        `${URL}?q=${newImgName}&page=${pageNum}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`,
+      )
+        .then(res => res.json())
+        .then(
+          imgCollection => this.props.onFetch(imgCollection.hits),
+          //   this.setState({ imgCollection: imgCollection.hits }),
+        );
+
+      //   this.props.onFetch(this.state.imgCollection);
+    }
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default GetGallery;
