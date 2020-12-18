dce3cfdff37cd6acba93b62623d970b4f935db15
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2020-12-18 15:58:11 +0200

    змінює на Компонент-функцію, додає onClick

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
 import s from './Button.module.css';
 
-export class Button extends Component {
-  state = {
-    page: 0,
-  };
-  render() {
-    return (
-      <div className={s.button__box}>
-        <button className={s.button} type="button">
-          Load more
-        </button>
-      </div>
-    );
-  }
+function Button({ onLoadMoreBtnClick }) {
+  return (
+    <div className={s.button__box}>
+      <button onClick={onLoadMoreBtnClick} className={s.button} type="button">
+        Load more
+      </button>
+    </div>
+  );
 }
 
 export default Button;
