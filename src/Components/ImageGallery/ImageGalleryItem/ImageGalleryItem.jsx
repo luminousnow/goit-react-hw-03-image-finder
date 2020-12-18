46764c2ad0a7447258f0215624fde4ee322cd1da
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2020-12-18 15:54:47 +0200

    править стилі додає Пропи

--- src/Components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/Components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import s from './ImageGalleryItem.molule.css';
+import s from './ImageGalleryItem.module.css';
 
 export class ImageGalleryItem extends Component {
   render() {
-    const smlImg = this.props.smlImg;
-    const lrgImg = this.props.lrgImg;
+    const smallImg = this.props.smallImg;
+    const largeImg = this.props.largeImg;
+    const alt = this.props.alt;
+    const onClick = this.props.onClick;
+
     return (
       <li className={s.imageGalleryItem}>
-        <img src={smlImg} alt="" className={s.imageGalleryItem__image} />
+        <img
+          src={smallImg}
+          data-original={largeImg}
+          onClick={onClick}
+          alt={alt}
+          className={s.imageGalleryItem__image}
+        />
       </li>
     );
   }
